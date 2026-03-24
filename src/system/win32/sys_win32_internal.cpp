881fc5a19506f388e1c1ec18b2609acdc4d630f8
Demez <demizexp@gmail.com>
2026-03-24 01:02:38 -0400

    win32: have char conversion funcs call other ones with size param, less code

--- src/system/win32/sys_win32_internal.cpp
+++ src/system/win32/sys_win32_internal.cpp
@@ -6,11 +6,13 @@
 // Internal String conversion functions
 
 
-wchar_t* sys_to_wchar( const char* spStr )
+wchar_t* sys_to_wchar( const char* spStr, int sSize )
 {
-	int      sSize = MultiByteToWideChar( CP_UTF8, 0, spStr, -1, NULL, 0 );
-
 	wchar_t* spDst = (wchar_t*)malloc( ( sSize + 1 ) * sizeof( wchar_t ) );
+	
+	if ( !spDst )
+		return nullptr;
+
 	memset( spDst, 0, ( sSize + 1 ) * sizeof( wchar_t ) );
 
 	MultiByteToWideChar( CP_UTF8, 0, spStr, -1, spDst, sSize );
@@ -21,14 +23,31 @@ wchar_t* sys_to_wchar( const char* spStr )
 }
 
 
-wchar_t* sys_to_wchar( const char* spStr, int sSize )
+wchar_t* sys_to_wchar( const char* spStr )
 {
-	wchar_t* spDst = (wchar_t*)malloc( ( sSize + 1 ) * sizeof( wchar_t ) );
+	int size = MultiByteToWideChar( CP_UTF8, 0, spStr, -1, NULL, 0 );
+
+	if ( size )
+		return sys_to_wchar( spStr, size );
+
+	return nullptr;
+}
+
+
+// Allows you to bypass the MAX_PATH limit in windows
+wchar_t* sys_to_wchar_extended( const char* spStr, int sSize )
+{
+	wchar_t* spDst = (wchar_t*)malloc( ( sSize + 5 ) * sizeof( wchar_t ) );
+
+	if ( !spDst )
+		return nullptr;
+
 	memset( spDst, 0, ( sSize + 1 ) * sizeof( wchar_t ) );
+	wcscat( spDst, L"\\\\?\\" );
 
-	MultiByteToWideChar( CP_UTF8, 0, spStr, -1, spDst, sSize );
+	MultiByteToWideChar( CP_UTF8, 0, spStr, -1, spDst + 4, sSize );
 
-	mem_add_item( e_mem_category_string, spDst, ( sSize + 1 ) * sizeof( wchar_t ), 1 );
+	mem_add_item( e_mem_category_string, spDst, ( sSize + 5 ) * sizeof( wchar_t ), 1 );
 
 	return spDst;
 }
@@ -37,53 +56,22 @@ wchar_t* sys_to_wchar( const char* spStr, int sSize )
 // Allows you to bypass the MAX_PATH limit in windows
 wchar_t* sys_to_wchar_extended( const char* spStr )
 {
-	int      sSize = MultiByteToWideChar( CP_UTF8, 0, spStr, -1, NULL, 0 );
+	int size = MultiByteToWideChar( CP_UTF8, 0, spStr, -1, NULL, 0 );
 
-	wchar_t* spDst = (wchar_t*)malloc( ( sSize + 5 ) * sizeof( wchar_t ) );
-	memset( spDst, 0, ( sSize + 1 ) * sizeof( wchar_t ) );
-	wcscat( spDst, L"\\\\?\\" );
+	if ( size )
+		return sys_to_wchar_extended( spStr, size );
 
-	MultiByteToWideChar( CP_UTF8, 0, spStr, -1, spDst + 4, sSize );
-
-	mem_add_item( e_mem_category_string, spDst, ( sSize + 5 ) * sizeof( wchar_t ), 1 );
-
-	return spDst;
-}
-
-
-// Allows you to bypass the MAX_PATH limit in windows
-wchar_t* sys_to_wchar_extended( const char* spStr, int sSize )
-{
-	wchar_t* spDst = (wchar_t*)malloc( ( sSize + 5 ) * sizeof( wchar_t ) );
-	memset( spDst, 0, ( sSize + 1 ) * sizeof( wchar_t ) );
-	wcscat( spDst, L"\\\\?\\" );
-
-	MultiByteToWideChar( CP_UTF8, 0, spStr, -1, spDst + 4, sSize );
-
-	mem_add_item( e_mem_category_string, spDst, ( sSize + 5 ) * sizeof( wchar_t ), 1 );
-
-	return spDst;
-}
-
-
-char* sys_to_utf8( const wchar_t* spStr )
-{
-	int   sSize = WideCharToMultiByte( CP_UTF8, 0, spStr, -1, NULL, 0, NULL, NULL );
-
-	char* spDst = (char*)malloc( ( sSize + 1 ) * sizeof( char ) );
-	memset( spDst, 0, ( sSize + 1 ) * sizeof( char ) );
-
-	int ret = WideCharToMultiByte( CP_UTF8, 0, spStr, -1, spDst, sSize, NULL, NULL );
-
-	mem_add_item( e_mem_category_string, spDst, ( sSize + 1 ) * sizeof( char ), 1 );
-
-	return spDst;
+	return nullptr;
 }
 
 
 char* sys_to_utf8( const wchar_t* spStr, int sSize )
 {
 	char* spDst = (char*)malloc( ( sSize + 1 ) * sizeof( char ) );
+
+	if ( !spDst )
+		return nullptr;
+
 	memset( spDst, 0, ( sSize + 1 ) * sizeof( char ) );
 
 	WideCharToMultiByte( CP_UTF8, 0, spStr, -1, spDst, sSize, NULL, NULL );
@@ -93,3 +81,13 @@ char* sys_to_utf8( const wchar_t* spStr, int sSize )
 	return spDst;
 }
 
+
+char* sys_to_utf8( const wchar_t* spStr )
+{
+	int size = WideCharToMultiByte( CP_UTF8, 0, spStr, -1, NULL, 0, NULL, NULL );
+
+	if ( size )
+		return sys_to_utf8( spStr, size );
+
+	return nullptr;
+}
